69c6a5e72c20541838d51bb1587d6d45b9e5bad1
Gerson <gerson.lcc@hotmail.com>
2017-09-26 21:59:04 +0100

    App complete

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerValue: UILabel!
+    
+    var timer = Timer()
+    
+    var timerCountdown = 210
+    
+    @objc func processedTimer()  {
+        if timerCountdown == 0 {
+            timer.invalidate()
+        } else {
+            timerCountdown = timerCountdown - 1
+            timerValue.text = String(timerCountdown)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timerValue.text = String(timerCountdown)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pauseBtnPressed(_ sender: UIBarButtonItem) {
+        timer.invalidate()
+    }
+    
+    @IBAction func startBtnPressed(_ sender: UIBarButtonItem) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processedTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func less10BtnPressed(_ sender: UIBarButtonItem) {
+        if timerCountdown <= 0 {
+            timerCountdown = 0
+            timerValue.text = String(timerCountdown)
+            timer.invalidate()
+        } else {
+        timerCountdown = timerCountdown - 10
+        timerValue.text = String(timerCountdown)
+        }
+    }
+    
+    @IBAction func resetBtnPressed(_ sender: UIBarButtonItem) {
+        timerCountdown = 210
+        timerValue.text = String(timerCountdown)
+    }
+    
+    @IBAction func plus10BtnPressed(_ sender: UIBarButtonItem) {
+        timerCountdown = timerCountdown + 10
+        timerValue.text = String(timerCountdown)
+    }
+    
+    
 }
 
